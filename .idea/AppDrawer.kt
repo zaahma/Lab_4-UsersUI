567039bc9e7f5f034c081baa991c17c282c599dd
zaahma <134698156+zaahma@users.noreply.github.com>
2024-05-07 10:27:33 +0300

    Update AppDrawer.kt

--- .idea/AppDrawer.kt
+++ .idea/AppDrawer.kt
@@ -31,3 +31,69 @@ fun AppDrawerHeaderPreview(){
         AppDrawerHeader()
     }
 }
+Text(text = "Заметки",
+modifier = Modifier
+.align(alignment = Alignment.CenterVertically))
+}
+@Preview
+@Composable
+fun AppDrawerHeaderPreview(){
+    NotesTheme {
+        AppDrawerHeader()
+    }
+}
+
+private fun ScreenNavigationButton(
+        icon: ImageVector,
+        label: String,
+        isSelected: Boolean,
+        onClick: () -> Unit
+) {
+    val colors = MaterialTheme.colors
+    val imageAlpha = if (isSelected){
+        1f
+    } else{
+        0.6f
+    }
+    val textColor = if (isSelected){
+        colors.primary
+    } else{
+        colors.onSurface.copy(alpha = 0.6f)
+    }
+
+    val backgroundColor = if (isSelected){
+        colors.primary.copy(alpha = 0.12f)
+    } else{
+        colors.surface
+    }
+    Surface(
+            modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 8.dp, end = 8.dp, top = 8.dp),
+            color = backgroundColor,
+            shape = MaterialTheme.shapes.small
+    ) {
+        Row(
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                        .clickable(onClick = onClick)
+                        .fillMaxWidth()
+                        .padding(4.dp)
+        ) {
+            Image(
+                    imageVector = icon ,
+                    contentDescription = "Screen Navigation Button",
+                    colorFilter = ColorFilter.tint(textColor),
+                    alpha = imageAlpha
+            )
+            Spacer(Modifier.width(16.dp))
+            Text(
+                    text = label,
+                    style = MaterialTheme.typography.body2,
+                    color = textColor,
+                    modifier = Modifier.fillMaxWidth()
+            )
+        }
+    }
+}
